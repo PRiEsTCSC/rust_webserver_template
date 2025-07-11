e4586bfd53a08f5d2812456dd2009a3ea1e6cd32
PRiEsTCSC <132480422+PRiEsTCSC@users.noreply.github.com>
2025-07-12 00:11:01 +0100

    fixing some errors

--- src/middleware/cors_mgt.rs
+++ src/middleware/cors_mgt.rs
@@ -10,7 +10,5 @@ pub fn handle_cors() -> Cors {
         .allow_any_origin()
         .allow_any_method()
         .allow_any_header()
-<<<<<<< HEAD
      // .allowed_origin(&allowed_origin)
-=======
->>>>>>> 42757935330962dcda83f603efb223dd589a3de0
+
