bc0e1c955d32190d12c313f91dc4ac5204eab182
PRiEsTCSC <132480422+PRiEsTCSC@users.noreply.github.com>
2025-07-12 00:25:36 +0100

    fixing some errors

--- src/middleware/cors_mgt.rs
+++ src/middleware/cors_mgt.rs
@@ -1,14 +1,13 @@
 use actix_cors::Cors;
+use actix_web::http::header;
 use std::env;
 
 pub fn handle_cors() -> Cors {
-    dotenv::dotenv().ok();
-    let mut allowed_origin = env::var("FRONTEND_URL").expect("FRONTEND_URL is not set");
+    let allowed_origin = env::var("FRONTEND_URL").expect("FRONTEND_URL is not set");
 
-    }
-    Cors::permissive
-        .allow_any_origin()
-        .allow_any_method()
-        .allow_any_header()
-     // .allowed_origin(&allowed_origin)
-
+    Cors::default()
+        .allowed_origin(&allowed_origin)
+        .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+        .allowed_headers(vec![header::AUTHORIZATION, header::CONTENT_TYPE])
+        .supports_credentials()
+}
